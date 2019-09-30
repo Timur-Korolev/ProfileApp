45c178ec91cf4d8c5bf2c6470d05136571200df1
Timur Korolev <timurkorolev89@gmail.com>
2019-09-30 19:59:25 +0300

    Add Tab Bar Controller

--- ProfileApp/ViewControllers/ViewController.swift
+++ ProfileApp/ViewControllers/ViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var showMoreButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        showMoreButton.layer.cornerRadius = 5
     }
 
 
